fdb0b311bdf4560f9690d793acd3ee91050db14a
EldarHasanli-maker <76447592+EldarHasanli-maker@users.noreply.github.com>
2021-10-23 10:34:30 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -22,12 +22,19 @@ var ball = {
 }
 
 function setup(){
-  var canvas =  createCanvas(700,600);
+canvas =  createCanvas(700,600);
+canvas.parent('canvas');
+video=createCapture(VIDEO);
+video.size(700,600);
+video.hide();
+poseNet=ml5.poseNet(video,modelLoaded);
 }
 
-
+function modelLoaded(){
+  console.log("model is initialized")
+}
 function draw(){
-
+image(video,0,0,700,600);
  background(0); 
 
  fill("black");
